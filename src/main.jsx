5074445243f42a59d87f46c515d5d43a3a4ae91c
AsumVictor <Iamasum369@gmail.com>
2023-11-03 21:41:14 +0000

    <incomplete

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/default.css'
+import './assets/css/style.css'
+// import './assets/js/main.js'
+ import './assets/js/scrolling-nav.js'
+// import './assets/js/vendor/jquery-1.12.4.min.js'
+// import './assets/js/vendor/modernizr-3.7.1.min.js'
+// import './assets/js/bootstrap.min.js'
+// import './assets/js/jquery.easing.min.js'
+// import './assets/js/jquery.magnific-popup.min.js'
+// import './assets/js/popper.min.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from '../components/header/Header.jsx'
+
+function HomeScreen() {
+  return (
+    <Header />
+    
+  );
+}
+
+export default HomeScreen;
