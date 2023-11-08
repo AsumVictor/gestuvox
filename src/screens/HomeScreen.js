d65d320c4dce1e33ace7c18e89de46d747d4944a
AsumVictor <Iamasum369@gmail.com>
2023-11-08 15:47:18 +0000

    feat: added how it works video

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,22 +6,24 @@ import PlayButton from '../components/buttons/videoPlayButton.js'
 
 function HomeScreen() {
   const Header = tw.div`py-4 bg-[#ffffff]`;
-  const MissionText = tw.h2` underline font-semibold text-primary-400 text-2xl md:text-3xl text-center`;
-  const HowItStartedHeader = tw.h3` font-bold text-center text-2xl text-primary-400`;
-  const VideoFram = tw.div` w-full py-2 px-3 lg:px-10 mt-10`;
+  const TextHeader = tw.h2` underline font-semibold text-primary-400 text-2xl md:text-3xl text-center`;
+  const VideoFram = tw.div`my-10 w-full py-2 px-3 lg:px-10 mt-10`;
   const VideoContainer = tw.div`bg-primary-200 h-[10cm] w-full rounded-md flex justify-center items-center`;
 
   return (
     <Header className=" mt-10">
       <Hero />
-      <MissionText>About Us</MissionText>
+      <TextHeader>About Us</TextHeader>
       <Mission />
-      <HowItStartedHeader>How It Works</HowItStartedHeader>
+      <TextHeader>How It Works</TextHeader>
       <VideoFram>
         <VideoContainer>
           <PlayButton />
         </VideoContainer>
       </VideoFram>
+      <TextHeader>
+        Future Plans
+      </TextHeader>
     </Header>
   );
 }
